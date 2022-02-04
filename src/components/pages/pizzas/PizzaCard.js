7f5152eff2e72cb7c18569edfa784945938a0b39
Unzueta Fernando <unzferk@gmail.com>
2022-02-04 08:46:50 -0400

    base

---
+++ src/components/pages/pizzas/PizzaCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const PizzaCard = ({_id,name,toppings=[]}) => {
+  return (
+    <>
+        <div key={_id} className="card w-50">
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p className="card-text">{toppings}</p>
+            <a href="#" className="btn btn-warning">Edit</a>
+            <a href="#" className="btn btn-danger">Delete</a>
+          </div>
+        </div>
+    </>
+  );
+};
--- src/components/pages/pizzas/PizzaScreen.js
+++ src/components/pages/pizzas/PizzaScreen.js
@@ -1,35 +1,43 @@
 import React from 'react';
+import { useEffect, useState } from "react";
+import { PizzaCard } from './PizzaCard';
+
 
 export const PizzaScreen = () => {
-  return (
-    <h1>PIZZA SCREEN</h1>  
-  )
-};
-/*
-import { useEffect, useState } from "react";
+
   const [post, setPost] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:4000/api/pizzas")
-    .then(response => response.json())
-    .then(data =>{
-      if(data){
-        console.log(data.pizzas);
-        setPost(data.pizzas);
-      }
-    });
+      .then(response => response.json())
+      .then(data => {
+        if (data) {
+          console.log(data.pizzas);
+          setPost(data.pizzas);
+        }
+      });
   }, []);
-  
 
   return (
     <>
-      <h1>Hi world</h1>
-      <hr></hr>
-      <ul>
-        {post && post.map( e=>(<li key={e._id}> {e.name}</li>))}
-      </ul>
-      
+      <div className='container mt-3'>
+
+        <div className='row '>
+          <form className="row g-3 d-flex justify-content-end">
+            <div className="col-auto">
+              <input type="text" className="form-control-plaintext" id="name" value="" placeholder='Name' />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary mb-3">Create</button>
+            </div>
+          </form>
+        </div>
+
+        <div className='row d-flex row-cols-auto'>
+          {post && post.map(e => (<PizzaCard key={e.id} name={e.name} toppings={e.toppings}/> ))}
+        </div>
+        
+      </div>
     </>
   );
-
-*/
+};
