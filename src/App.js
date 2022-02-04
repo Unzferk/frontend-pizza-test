96e7c56c21dbed647da98767bc82feef6467d0ed
Unzueta Fernando <unzferk@gmail.com>
2022-02-03 23:15:15 -0400

    initial status

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/pizzas")
+    .then(response => response.json())
+    .then(data =>{
+      if(data){
+        console.log(data.pizzas);
+        setPost(data.pizzas);
+      }
+    });
+  }, []);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hi world</h1>
+      <hr></hr>
+      <ul>
+        {post && post.map( e=>(<li key={e._id}> {e.name}</li>))}
+      </ul>
+      
+    </>
   );
 }
 
